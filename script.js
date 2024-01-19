e2d250300b9b610456bd9c5c02d976b2125903c1
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-01-19 17:51:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+/*************************************************************************
+ updateTime fuction is used to update the time of the clock. It show the updates the time in DOM as well as generate the greeting dinamically according to the time of the day
+**************************************************************************/
+function updateTime(){
+    const HR = document.querySelector("#hr-container > div > p");
+    const MIN = document.querySelector("#min-container > div > p");
+    const SEC = document.querySelector("#sec-container > div > p");
+    const MERIDIEM = document.querySelector("#meridiem");
+    const GREETING = document.querySelector("#greeting");
+
+    let currTime = new Date();
+    let currHour = currTime.getHours();
+    let currMin = currTime.getMinutes();
+    let currSec = currTime.getSeconds();
+
+    if(currHour < 12){
+        MERIDIEM.innerText = "AM";
+        GREETING.innerText = "GOOD MORNING!!!"
+    } else {
+        MERIDIEM.innerText = "PM";
+        if(currHour < 17){
+            GREETING.innerText = "GOOD AFTERNOON!!!"
+        } else {
+            GREETING.innerText = "GOOD EVENING!!!"
+        }
+
+    }
+
+    HR.innerText = currHour % 12 < 10 ? "0" + currHour % 12 : currHour % 12;
+    MIN.innerText = currMin < 10 ? "0" + currMin : currMin;
+    SEC.innerText = currSec < 10 ? "0" + currSec : currSec;
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    setInterval(updateTime,10);
+})
